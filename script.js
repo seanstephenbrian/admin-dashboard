a2c13410f1f48e71e5ca34d5efd086906d1eea66
sean stephen brian <seanstephenbrian@gmail.com>
2022-09-20 08:53:48 -0400

    Lay out content within project cards, add icons

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function generateProjectIcons() {
     const projectIcons = document.querySelectorAll('.project-icons');
     projectIcons.forEach(div => {
-        div.innerHTML = 'a b c';
+        div.innerHTML = 
+            `<img src="img/star.svg" alt="Star icon">
+            <img src="img/edit.svg" alt="Edit icon">
+            <img src="img/share.svg" alt="Share icon">`;
     });
 }
 
