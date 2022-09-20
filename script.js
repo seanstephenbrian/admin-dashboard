dd278d4ed77e61a96f5d5d5562df1266cc3c0e06
sean stephen brian <seanstephenbrian@gmail.com>
2022-09-20 17:14:41 -0400

    Complete visual design of project cards

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function generateProjectIcons() {
     const projectIcons = document.querySelectorAll('.project-icons');
     projectIcons.forEach(div => {
         div.innerHTML = 
-            `<img src="img/star.svg" alt="Star icon">
-            <img src="img/edit.svg" alt="Edit icon">
-            <img src="img/share.svg" alt="Share icon">`;
+            `<img class="star" src="img/star.svg" alt="Star icon">
+            <img class="edit" src="img/edit.svg" alt="Edit icon">
+            <img class="share" src="img/share.svg" alt="Share icon">`;
     });
 }
 
